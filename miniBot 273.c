32a0b01801c4c805f78b49d44df4d564c6a104af
papercarrot <37120970+papercarrot@users.noreply.github.com>
2018-03-28 09:20:04 +0300

    Add files via upload
    This is the up to date code so far, please upload the next codes after you've edited them here

---
+++ miniBot 273.c
@@ -0,0 +1,16 @@
+#include <kipr/botball.h>
+
+int main()
+{
+    speed = 100;
+    bkspeed=-100;
+    rest=0;
+    enable_servos();
+    camera_open_black();
+    return 0;
+}
+
+void servos_open();{
+	set_servo_position(0,299);
+    set_servo_position(1,729);
+}
